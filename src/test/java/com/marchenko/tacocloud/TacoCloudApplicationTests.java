8816e578cd77ebb0fa07f091ece8413c3ef95f07
Vitalii Marchenko <vitalymarchenko@Vitalys-MacBook-Pro.local>
2020-06-12 09:33:36 +0300

    Add initial project structure

---
+++ src/test/java/com/marchenko/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,34 @@
+package com.marchenko.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+@WebMvcTest
+class TacoCloudApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void testHomePage() throws Exception {
+		mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("home"))
+				.andExpect((ResultMatcher) content().string(containsString("Welcome to...")));
+	}
+
+}
